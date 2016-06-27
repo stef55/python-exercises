b915ae27fb7466f8151d02cd8598e9e6869107ff
stef55 <corettistefano@gmail.com>
2016-06-27 09:17:03 +0200

    create a list and print the name+"is awesome"

--- awesomeness.py
+++ awesomeness.py
@@ -1,5 +1,5 @@
 """create a list made by 5 name and add them to a list with wroten "is awesome"""
-names=['Brian','Zac','Cody','Oronzo','icannotfindthelastname']
+names=['Brian','Zac','Cody','Oronzo','randomname']
 awesome_names=[name +' '+'is awesome' for name in names]
 for awesome_name in awesome_names:
     print(awesome_name)
