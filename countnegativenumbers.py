d9c3de68c98813f5ee8d0aefb25a9da8f08e01e5
stef55 <corettistefano@gmail.com>
2016-06-27 09:22:12 +0200

    count the number of negative and positive numbers

--- countnegativenumbers.py
+++ countnegativenumbers.py
@@ -1,9 +1,9 @@
 #check negative numbers and count them
 numbers=[1,2,-1,-8,-7,15,22,23,167]
 #number of negative numbers
-n=0
+nega=0
 #number of positive numbers
-p=0
+pos=0
 for number in numbers:
     if number<0:
         n=n+1
