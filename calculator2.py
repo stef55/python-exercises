42e78839a1967391927baa93568a1c293a39debc
stef55 <corettistefano@gmail.com>
2016-06-27 09:18:52 +0200

    calculate the sum of 2 number outside a function

--- calculator2.py
+++ calculator2.py
@@ -2,7 +2,7 @@
 def calculator(n1,n2):
     r=n1+n2
     return r
-numbers={3:2,4:11,1:18}
+numbers={3:2,4:11,1:13}
 for key,value in numbers.items():
     result=calculator(key,value)
     print(str(key)+'+'+str(value)+'='+ str(result))  
